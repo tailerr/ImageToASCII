3212c10881abed31e8b36070fdae60af57c5927c
alexa <alexa@LAPTOP-BSCP12KB>
2017-09-24 21:45:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 3 || args[0]=="-help")
+                {
+                    help();
+                    return;
+
+                }
+                string filename = args[0];
+                string path = args[1];
+                double k = Convert.ToDouble(args[2]);
+                if (k<=0 || k > 1)
+                {
+                    Console.WriteLine("k value should be in (0, 1]");
+                    Console.ReadLine();
+                    return;
+                }
+                if (args.Length < 4)
+                {
+                    converter conv = new converter(filename, path, k);
+                    conv.procces(path);
+                }
+                else
+                {
+                    string alf = ' '+args[3];
+                    converter conv = new converter(filename, path, k, alf);
+                    conv.procces(path);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("wrong type of values");
+                Console.ReadKey();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("file or directory does not exist");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("check your input");
+                Console.ReadKey();
+            }
+            
+        }
+
+        static void help()
+        {
+            Console.WriteLine("HELP");
+            Console.WriteLine("This program needs three required parametrs and one optional:");
+            Console.WriteLine("First is a path to image you want to convert");
+            Console.WriteLine("    supported formats:  BMP, GIF, EXIF, JPG, PNG и TIFF");
+            Console.WriteLine("Second is a path where program will save output");
+            Console.WriteLine("    supported formats: text formats");
+            Console.WriteLine("Third is a compression parametr defined in (0, 1] (1=full size)");
+            Console.WriteLine("Last optional is an alphabet of ASCII symbols ordered from white to black");
+            Console.WriteLine("    space to alphabet adds automatically");
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ConsoleApplication1/converter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ConsoleApplication1
+{
+    class converter
+    {
+        Bitmap bitmap;
+        string alphabet; 
+        string path;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="f">path to image you want to convert</param>
+        /// <param name="alp">alphabet of ASCII symbols ordered from 'white' to 'black'</param>
+        /// <param name="Path">path where program will save output</param>
+        /// <param name="k">compression parametr defined in (0, 1] (1=full size)</param>
+        public converter(string f, string Path, double k, string alp = " .,:;ox%#@")
+        {
+            bitmap = new Bitmap(f);
+            alphabet = alp;
+            path = Path;
+            if (k != 1)
+            {
+                bitmap = ResizeImage(bitmap, Convert.ToInt16(bitmap.Width * k), Convert.ToInt16(bitmap.Width * k));
+            }
+            bitmap.RotateFlip(RotateFlipType.Rotate270FlipY);
+        }
+
+        /// <summary>
+        /// Resize the image if needs
+        /// </summary>
+        /// <param name="image">bitmap to resize</param>
+        /// <param name="width">new width of image</param>
+        /// <param name="height">new height of image</param>
+        /// <returns></returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+
+        /// <summary>
+        /// The procces of convertation
+        /// </summary>
+        /// <param name="path">path where program will save output</param>
+        public void procces(string path)
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        Color c = bitmap.GetPixel(i, j);
+                        sw.Write(colorconv(c));
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+     
+        /// <summary>
+        /// Convertaion color to our ASCII alphabet  
+        /// </summary>
+        /// <param name="c">color</param>
+        /// <returns></returns>
+        public char colorconv(Color c)
+        {
+            double res = (c.B + c.R + c.G)/3.0/256;
+            double step = 1.0 / this.alphabet.Length;
+            int r = Convert.ToInt16(Math.Truncate(res / step));
+            return this.alphabet[alphabet.Length-1-r];
+        }
+    }
+}
